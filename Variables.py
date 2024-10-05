6907f5fb01a6aea77ea691c06ada67d9f14668ec
kazzmatos <kazzmatos@gmail.com>
2024-10-05 20:04:33 +0300

    Переменные

---
+++ Variables.py
@@ -0,0 +1,5 @@
+tasks_completed=int(12)
+hours_spent=1.5
+course_name='Python'
+task_time=(hours_spent/tasks_completed)
+print('Курс:',course_name,',''всего задач:',tasks_completed,',''затрачено часов:',hours_spent,',' 'среднее время выполнения:',task_time,'часа''.')
