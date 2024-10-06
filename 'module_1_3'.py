9d63e69466300cf51431d254e82be34d1385a747
kazzmatos <kazzmatos@gmail.com>
2024-10-06 17:22:39 +0300

    Динамическая типизация

--- 'module_1_3'.py
+++ 'module_1_3'.py
@@ -2,7 +2,7 @@ name='Alex'
 print('Name:', name)
 age=int(61)
 print('Age:', age)
-age=int(61+1)
+age=int(age+1)
 print('New Age:', age)
-is_student='True'
+is_student=True
 print('Is student:',is_student)
