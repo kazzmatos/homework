da61f505df0673050f88206b9eafe48010cd54d2
kazzmatos <kazzmatos@gmail.com>
2024-10-13 10:33:31 +0300

    Неизменяемые и изменяемые объекты. Кортежи.

---
+++ 'module_1_5'.py
@@ -0,0 +1,8 @@
+immutable_var=(2,3,5,'a','b',False)
+print('Immutable_tuple:',immutable_var)
+mutable_list=[1,16,'a','b']
+mutable_list[0]=2
+mutable_list[1]=10
+mutable_list[2]='c'
+mutable_list.append('Modified')
+print('Mutable list:',mutable_list)
