27a76cd8134a3c634875242a3d4ec36a7bf465e1
kazzmatos <kazzmatos@gmail.com>
2024-10-16 21:03:36 +0300

    Задание по модулю: Базовые структуры данных

---
+++ Students'_grade.py
@@ -0,0 +1,7 @@
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+keys=sorted(list(students))
+values=[sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
+my_dict=dict(zip(keys, values))
+print(my_dict)
