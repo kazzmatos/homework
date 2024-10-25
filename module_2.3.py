77f8211e290994111648038d0082a184dfc02000
kazzmatos <kazzmatos@gmail.com>
2024-10-26 00:10:39 +0300

    Цикл "While"

---
+++ module_2.3.py
@@ -0,0 +1,18 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+b = len(my_list)
+while my_list[a:b]:
+    if my_list[a] == 0:
+        a = a + 1
+        continue
+    print(my_list[a])
+    a = a + 1
+    if my_list[a] < 0:
+        break
+
+
+
+
+
+
+
